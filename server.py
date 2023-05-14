f52ef6fecacf4078d8d31afa63694dfb567e7a20
Jerry <jerryzhao173985@gmail.com>
2023-05-14 15:42:59 +0100

    Heroku

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify, make_response, Response, render_template
 import os
+from flask import Flask, request, jsonify, make_response, Response, render_template
 import cv2
 import pickle
 import socket
@@ -10,17 +10,15 @@ CONST_BASE_PATH = os.path.dirname(__file__)
 app = Flask(__name__)
 agent = PapersWithCode()
 
-
 @app.route("/pdf/<pdf_file>", methods=['GET'])
 def show_pdf(pdf_file):
     return render_template("pdf.html", filename=pdf_file, header=f"http://127.0.0.1:8084")
-
 
 @app.route("/")
 def main():
     data = agent.get()
     return render_template("index.html", paperinfo=data)
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8084)
+    port = int(os.environ.get("PORT", 8084))
+    app.run(host='0.0.0.0', port=port)
